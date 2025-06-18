aa9c1a92ab0aafe055f41c4e7570d5ef8b36ec3a
Amy Xiong <amyxiongg@gmail.com>
2025-06-18 13:59:30 -0700

    reverse linked list - iterative

--- reverse_linked_list.py
+++ reverse_linked_list.py
@@ -1,4 +1,3 @@
-# recursion
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -7,13 +6,12 @@
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
+        prev = None
+        curr = head
 
-        newHead = head
-        if head.next:
-            newHead = self.reverseList(head.next)
-            head.next.next = head
-        head.next = None
-        
-        return newHead
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
